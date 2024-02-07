7c539e631c4e25dc8d26bf1cc915952f1624e5ec
enjima7 <enjima07@gmail.com>
2024-02-07 00:13:34 -0600

    lazy load added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import { BrowserModule} from '@angular/platform-browser';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule} from '@angular/common/http';
+import { CardComponent } from './gifs/components/card/card.component';
 
 
 
 @NgModule({
   declarations: [
-  AppComponent
+  AppComponent,
   ],
   imports:[
     BrowserModule,
     HttpClientModule,
     GifsModule,
-    SharedModule
+    SharedModule,
   ],
   providers:[],
   bootstrap:[AppComponent]
---
+++ src/app/shared/components/lazy-Image/lazy-image.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html'
+})
+export class LazyImageComponent implements OnInit {
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('URL property is required');
+
+  }
+
+  public hasLoeaded: boolean = false;
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  onLoad(){
+    setTimeout(() =>{
+      this.hasLoeaded = true;
+    }, 500)
+
+  }
+
+
+}
